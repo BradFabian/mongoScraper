4fe51d5073bc8739f8a00ff8c42d1d8671c4cce5
Brad <bradleofabian@gmail.com>
2018-09-15 15:58:18 -0400

    set up dependencies server.js

---
+++ server.js
@@ -0,0 +1,9 @@
+// Dependencies
+var express = require("express");
+var mongojs = require("mongojs");
+// Require request and cheerio. This makes the scraping possible
+var request = require("request");
+var cheerio = require("cheerio");
+
+// Initialize Express
+var app = express();
